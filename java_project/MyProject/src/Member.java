7aff1dcec29e54145ea3781d5a95a06f512f5d99
yiwoori <yiwoori93@naver.com>
2021-05-24 18:02:21 +0900

    Update Member.java

--- java_project/MyProject/src/Member.java
+++ java_project/MyProject/src/Member.java
@@ -1,6 +1,16 @@
 public class Member {
 	public static void main(String[] args) {
 		System.out.println("이우리");
-
+		
+		String name = "이우리입니다";
+		int age = 29;
+		double height = 157.3;
+		boolean hasBooks = true;
+		
+		System.out.println();
+		System.out.println("이름 : "+name);
+		System.out.println("나이 : "+age);
+		System.out.println("키 : "+height);
+		System.out.println("책 보유 여부 : "+hasBooks);
 	}
 }
