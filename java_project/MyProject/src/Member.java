09860826d9c6c3245cb2abd18c7dd6738c77ca11
yiwoori <yiwoori93@naver.com>
2021-05-24 16:58:08 +0900

    Update Member.java

--- java_project/MyProject/src/Member.java
+++ java_project/MyProject/src/Member.java
@@ -1,20 +1,7 @@
-class Calculator {
-	int add(int a, int b) {
-		return a+b;
-	}
-	int subtract(int a, int b) {
-		return a-b;
-	}
-	int multiply(int a, int b) {
-		return a*b;
-	}
-	int divide(int a, int b) {
-		return a/b;
-	}
-}
+
 public class Member {
 	public static void main(String[] args) {
-		System.out.println("�̿츮");
+		System.out.println("이우리");
 
 	}
 }
