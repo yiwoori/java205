3eadc9fc24aef4168a07f4e3ee81f0bed3a0257c
yiwoori <yiwoori93@naver.com>
2021-05-24 15:56:01 +0900

    Create Member.java

---
+++ java_project/MyProject/src/Member.java
@@ -0,0 +1,20 @@
+class Calculator {
+	int add(int a, int b) {
+		return a+b;
+	}
+	int subtract(int a, int b) {
+		return a-b;
+	}
+	int multiply(int a, int b) {
+		return a*b;
+	}
+	int divide(int a, int b) {
+		return a/b;
+	}
+}
+public class Member {
+	public static void main(String[] args) {
+		System.out.println("�̿츮");
+
+	}
+}
