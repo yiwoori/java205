66ef06d3d3b270a8ad33bfa67488dcd46cf1c517
yiwoori <yiwoori93@naver.com>
2021-05-24 17:38:47 +0900

    Update Calculator.java

--- java_project/MyProject/src/Calculator.java
+++ java_project/MyProject/src/Calculator.java
@@ -1,7 +1,5 @@
 public class Calculator {
 
-	//메소드 -> 기능 (재료 - 처리 - 반환)
-	//반환타입 메소드이름(매개변수들) { 처리구문 }
 	void plus(int n1, int n2) {
 		System.out.println(n1+n2);
 	}
@@ -16,8 +14,6 @@ public class Calculator {
 	}
 	public static void main(String[] args) {
 		
-		//인스턴스 생성 -> 클래스의 정의 필요 (필요한 변수, 필요한 메소드)
-		//클래스이름 참조타입 참조변수 = new 클래스이름
 		Calculator cal = new Calculator();
 		cal.plus(10, 20);
 		cal.minus(20, 10);
