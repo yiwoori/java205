fc9958b7dcdaea1134c329a1ccae5d00dd505320
yiwoori <yiwoori93@naver.com>
2021-05-24 18:19:59 +0900

    Update Person.java

--- java_project/MyProject/src/Person.java
+++ java_project/MyProject/src/Person.java
@@ -5,4 +5,16 @@ public class Person {
 	String phoneNumber;
 	String idNumber;
 	
+	public static void main(String[] args) {
+		
+		Person p = new Person();
+		p.personName = "이우리";
+		p.phoneNumber = "010-1234-5678";
+		p.idNumber = "123456-0987654";
+		
+		System.out.println("이름 : "+p.personName);
+		System.out.println("번호 : "+p.phoneNumber);
+		System.out.println("주민등록번호 : "+p.idNumber);
+		
+	}
 }
