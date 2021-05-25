d7f87b56f953b6eda301b380d5e45d59962cd734
yiwoori <yiwoori93@naver.com>
2021-05-25 18:51:33 +0900

    Update Member.java

--- java_project/MyProject/src/ch05/Member.java
+++ java_project/MyProject/src/ch05/Member.java
@@ -5,24 +5,47 @@ public class Member {
 	String name, phoneNumber, major, email, address;
 	int grade, birthDay;
 	
-	void memberName(String name) {
+	void print1() {
 		System.out.println(name);
+		System.out.println(phoneNumber);
+		System.out.println(major);
+		System.out.println(email);
+		System.out.println(address);
+		System.out.println(grade);
+		System.out.println(birthDay);
+	}
+	void print2() {
+		System.out.println(name);
+		System.out.println(phoneNumber);
+		System.out.println(major);
+		System.out.println(email);
+		System.out.println(grade);
 	}
 	
 	public Member(String name, String phoneNumber,
-			String major, String email, String address) {
+			String major, String email, String address,
+			int grade, int birthDay) {
 		this.name = name;
 		this.phoneNumber = phoneNumber;
 		this.major = major;
 		this.email = email;
 		this.address = address;
+		this.grade = grade;
+		this.birthDay = birthDay;
 	}
-	
+	public Member(String name, String phoneNumber,
+			String major, String email, int grade) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.major = major;
+		this.email = email;
+		this.grade = grade;
+	}
 	
 	public static void main(String[] args) {
 		
-		//Member m = new Member();
-
-
+		Member m = new Member("이우리","010-1234-5678","경영학과","yiwoori@gmail.com",4);
+		m.print2();
+		
 	}
 }
