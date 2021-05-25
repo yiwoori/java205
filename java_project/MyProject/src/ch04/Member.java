5e6251716e95a72beedb057604f5fc75334ac204
yiwoori <yiwoori93@naver.com>
2021-05-25 16:37:49 +0900

    Update Member.java

--- java_project/MyProject/src/ch04/Member.java
+++ java_project/MyProject/src/ch04/Member.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Member {
 	
-	static void vac (int age) {
+	void vac (int age) {
 		if(15>age||age>64) {
 			System.out.println("무료 예방접종이 가능합니다.");
 		} else {
@@ -11,7 +11,7 @@ public class Member {
 		}
 	}
 	
-	static void checkUp (int age) {
+	void checkUp (int age) {
 		if(age>19) {
 			for(int i=20; i<100; i++) {
 				if(age%2!=0 && age>39) {
@@ -30,9 +30,10 @@ public class Member {
 			System.out.println("검진 대상이 아닙니다.");	
 		}
 	}
-
 	
 	public static void main(String[] args) {
+		
+		Member m = new Member();
 		
 		Scanner scn = new Scanner(System.in);
 		System.out.println("나이를 입력해주세요 ▼ ");
@@ -40,11 +41,11 @@ public class Member {
 		
 		System.out.println();
 		System.out.println("<독감예방접종>");
-		vac(age);
+		m.vac(age);
 		
 		System.out.println();
 		System.out.println("<건강검진>");
-		checkUp(age);
+		m.checkUp(age);
 		
 	}
 }
