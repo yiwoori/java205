d5b231222401dbfbe5bf318759ef560e0d3c32e6
yiwoori <yiwoori93@naver.com>
2021-05-27 19:15:28 +0900

    Update Question3.java

--- java_project/MyProject/src/Array/Question3.java
+++ java_project/MyProject/src/Array/Question3.java
@@ -15,8 +15,25 @@ package Array;
 
 public class Question3 {
 	
-	public static void arr(int[][] arr, int a) {
+	public static void arr(int[][] arr) {
 		
+		int[] arrTem = {};
+		for(int i=0; i<arr[i].length; i++) {
+			arrTem = arr[0];
+			arr[0] = arr[1];
+			arr[arr.length-1] = arrTem;
+		} //System.out.println(arr[i]);
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		int[][] arr = {
+				{1,2,3}, {4,5}, {6,7,8,9}
+		};
+		arr(arr);
 		
 		
 	}
