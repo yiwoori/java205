d6d9caa2be26fe521da20e1b7243148a38127811
yiwoori <yiwoori93@naver.com>
2021-05-31 17:31:33 +0900

    .

--- java_project/MyProject/src/ch08/Calculator.java
+++ java_project/MyProject/src/ch08/Calculator.java
@@ -2,7 +2,7 @@ package ch08;
 
 interface Calculator {
 	long add(long n1, long n2);
-	long subsrtact(long n1, long n2);
+	long substract(long n1, long n2);
 	long multiply(long n1, long n2);
-	double divibe(long n1, long n2);
+	double divide(long n1, long n2);
 }
--- java_project/MyProject/src/ch08/CalculatorMain.java
+++ java_project/MyProject/src/ch08/CalculatorMain.java
@@ -1,7 +1,24 @@
 package ch08;
 
+import java.util.Scanner;
+
 public class CalculatorMain {
 	public static void main(String[] args) {
+		
+		Calculator cal = new Calculator2();
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.print("첫번째 숫자 입력 : ");
+		long n1 = scn.nextLong();
+		System.out.print("두번째 숫자 입력 : ");
+		long n2 = scn.nextLong();
+		
+		System.out.println("================================");
+		
+		System.out.println("더한 값 : "+cal.add(n1, n2));
+		System.out.println("뺀 값 : "+cal.substract(n1, n2));
+		System.out.println("곱한 값 : "+cal.multiply(n1, n2));
+		System.out.println("나눈 값 : "+cal.divide(n1, n2));
 		
 		
 		
