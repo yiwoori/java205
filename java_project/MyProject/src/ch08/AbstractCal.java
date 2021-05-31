9a857d7db548ecf33e514c5087817d4f916e5fae
yiwoori <yiwoori93@naver.com>
2021-05-31 15:52:00 +0900

    .1

---
+++ java_project/MyProject/src/ch08/AbstractCal.java
@@ -0,0 +1,11 @@
+package ch08;
+
+public abstract class AbstractCal implements Calculator {
+	public long add (long n1, long n2) {
+		return n1+n2;
+	}
+	public long substract(long n1, long n2) {
+		return n1-n2;
+	}
+
+}
---
+++ java_project/MyProject/src/ch08/Calculator.java
@@ -0,0 +1,8 @@
+package ch08;
+
+interface Calculator {
+	long add(long n1, long n2);
+	long subsrtact(long n1, long n2);
+	long multiply(long n1, long n2);
+	double divibe(long n1, long n2);
+}
