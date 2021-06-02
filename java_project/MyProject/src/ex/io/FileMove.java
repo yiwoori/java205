593cf9ef79f382052634b63927fba90fc86d7286
yiwoori <yiwoori93@naver.com>
2021-06-02 17:01:03 +0900

    Update FileMove.java

--- java_project/MyProject/src/ex/io/FileMove.java
+++ java_project/MyProject/src/ex/io/FileMove.java
@@ -21,11 +21,21 @@ public class FileMove {
 		if(!newDir.exists()) {
 			//폴더 생성
 			newDir.mkdir();
+			System.out.println("2. exist ?? --> "+newDir.exists());
 		}
 		
-		System.out.println("2. exist ?? --> "+newDir.exists());
+		System.out.println(myFile.getName());
 		
-
+		//이동할 경로와 파일 이름
+		File newFile = new File(newDir, myFile.getName());
+		//이동
+		myFile.renameTo(newFile);
+		
+		if(newFile.exists()) {
+			System.out.println("파일 이동 성공");
+		} else {
+			System.out.println("파일 이동 실패");
+		}
 		
 		
 	}
