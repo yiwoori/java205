cbfaf2c6d56b731d29b83ba3c2686e7b94f83620
yiwoori <yiwoori93@naver.com>
2021-06-02 16:10:52 +0900

    .

--- java_project/MyProject/src/ex/io/ObjectSerializable.java
+++ java_project/MyProject/src/ex/io/ObjectSerializable.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class ObjectSerializable {
 	public static void main(String[] args) {
@@ -14,21 +15,42 @@ public class ObjectSerializable {
 			ObjectOutputStream out = new ObjectOutputStream
 					(new FileOutputStream("Object.ser"));
 			
-			out.writeObject(new Circle(1,1,2.4));
+			out.writeObject(new Circle(1,1,3.4));
 			out.writeObject(new Circle(3,5,7.9));
 			out.writeObject("Hello");
+			
+			ArrayList<Circle> list = new ArrayList<>();
+			list.add(new Circle(1, 2, 3.4));
+			list.add(new Circle(5, 6, 7.8));
+			list.add(new Circle(9, 12, 13.14));
+			list.add(new Circle(21, 22, 23.24));
+			list.add(new Circle(31, 32, 33.34));
+			out.writeObject(list);
+			
 			out.close();
 			
 			System.out.println("인스턴스 저장 완료");
 			
 			//인스턴스 복원
 			ObjectInputStream in = new ObjectInputStream
-					(new FileInputStream("Object"));
+					(new FileInputStream("Object.ser"));
 			
 			//복원 순서는 저장 순서에 맞게 복원
 			Circle c1 = (Circle) in.readObject();
 			Circle c2 = (Circle) in.readObject();
 			String str = (String) in.readObject();
+			
+			ArrayList<Circle> list2 = (ArrayList<Circle>) in.readObject();
+			
+			for(Circle circle : list2) {
+				System.out.println(circle);;
+			}
+			
+			System.out.println("복원된 인스턴스의 데이터를 출력");
+			
+			c1.showData();
+			c2.showData();
+			System.out.println(str);
 			
 			
 		} catch (IOException e) {
