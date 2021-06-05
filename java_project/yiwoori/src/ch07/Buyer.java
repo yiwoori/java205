a48f3eb644519d315eff22d883f1ae48d33fd35d
yiwoori <yiwoori93@naver.com>
2021-06-05 18:22:00 +0900

    polymorphism

--- java_project/yiwoori/src/ch07/Buyer.java
+++ java_project/yiwoori/src/ch07/Buyer.java
@@ -5,19 +5,32 @@ public class Buyer {
 	
 	int money = 10000;
 	int bonusPoint = 0;
+	Product[] cart = new Product[10];
+	int i = 0;
 	
 	void buy(Product p) {
-
 		if(money<p.price) {
 			System.out.println("잔액이 부족합니다.");
 			return;
 		} else {
 			money -= p.price;
 			bonusPoint += p.bonusPoint;
+			cart[i++] = p;
 			System.out.println(p+"구매 완료");
 		}
+	}
+	
+	void sum() {
+		int sum = 0;
+		String itemString = "";
 		
-		
+		for(int i=0; i<cart.length; i++) {
+			if(cart[i]==null ) { break; }
+			sum += cart[i].price;
+			itemString += cart[i] + ", ";
+		}
+		System.out.println("총 금액은 "+sum+"원 입니다.");
+		System.out.println("구매 제품은 "+itemString+"입니다.");
 	}
 	
 	
--- java_project/yiwoori/src/ch07/BuyerMain.java
+++ java_project/yiwoori/src/ch07/BuyerMain.java
@@ -7,6 +7,7 @@ public class BuyerMain {
 		b1.buy(new Tv());
 		b1.buy(new Audio());
 		b1.buy(new Computer());
+		b1.sum();
 		
 		System.out.println("==========================");
 		System.out.println("현재 남은 금액은 "+b1.money+"원 입니다.");
@@ -19,7 +20,7 @@ public class BuyerMain {
 		}
 		
 		System.out.println("보유 중인 포인트는 "+b1.bonusPoint+"점 입니다.");
-			
+		System.out.println("==========================");
 		
 	}
 }
