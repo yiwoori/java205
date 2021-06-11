52d3bf3e0b832ae07596cfba7a8c3035a5edaf73
yiwoori <yiwoori93@naver.com>
2021-06-11 12:02:43 +0900

    sql

--- sql/20210610_sql_function.sql
+++ sql/20210610_sql_function.sql
@@ -60,6 +60,68 @@ select sysdate
 from dual; --오늘 날짜 출력
 
 
+-- 형변환 함수
+--
+
+
+select orderid, orderdate, to_char(orderdate, 'YYYY.MM.DD')
+from orders;
 
 
 
+-- 숫자 -> 문자
+select to_char(123456, '0000000000'), to_char(123456, '9999999999') --10자리를 만들고 6개까지만 출력되게
+from dual;
+select to_char(123456, '0,000,000,000'), to_char(123456, 'L9,999,999,999') --L:통화는 현재 위치 기반(시스템 설정)
+from dual;
+
+select empno, ename, to_char(sal*1100, 'L999,999,999')
+from emp;
+
+
+
+
+
+--'1,000,000'+100000
+
+-- 문자 -> 숫자, 문자 -> 날짜
+-- 1. 문자->숫자
+-- to_number(문자열, 패턴)
+select to_number('1,000,000', '9,999,999'),
+        to_number('1,000,000','9,999,999')+100000 --문자열을 숫자로 변환한후 연산 수행
+from dual;
+
+-- 2. 문자->날짜
+-- to_date(문자열, 패턴)
+select to_date('2012.05.17', 'YYYY.MM.DD'),
+    trunc(sysdate-to_date('2012.05.17', 'YYYY.MM.DD')) --trunc:소수점 삭제
+from dual;
+
+
+
+-- decode 함수 : 분기를 위해 사용. switch-case와 유사
+-- decode(컬럼, 조건1 값, 조건1이 참일때 사용할 값
+--            , 조건2 값, 조건2가 참이래 사용할 값, ...)
+select ename, deptno,
+        decode(deptno, 10, 'ACCOUNTING', 20, 'RESEARCH',
+                       30, 'SALES', 40, 'OPERATIONS') as DNAME
+from emp;
+
+
+select empno, ename, sal,
+    decode(job, 'ANALYST', sal*1.05, 'SALESMAN', sal*1.1,
+            'MANAGER', sal*1.15, 'CLERK', sal*1.2) as UPSAL
+from emp;
+
+
+
+-- case 함수도 분기할 때 사용
+-- case when 조건식 then 참일때 값
+select ename, deptno,
+        case when deptno=10 then 'ACCOUNTING'
+             when deptno=20 then 'REARCH'
+             when deptno=30 then 'SALES'
+             when deptno=40 then 'OPERATIONS'
+        End as DNAME
+from emp
+order by dname desc;
