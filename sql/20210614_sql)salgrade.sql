4489b87e1e29e10dddd727bc1707c467118503ad
yiwoori <yiwoori93@naver.com>
2021-06-14 12:29:46 +0900

    sql

--- sql/20210614_sql)salgrade.sql
+++ sql/20210614_sql)salgrade.sql
@@ -1,4 +1,21 @@
-
 -- 2021.06.14
 
---
+-- salgrade ddl
+
+drop table salgrade;
+
+CREATE TABLE SALGRADE
+(GRADE NUMBER,
+LOSAL NUMBER,
+HISAL NUMBER);
+
+--트랜젝션이 시작
+INSERT INTO SALGRADE VALUES (1, 700, 1200);
+INSERT INTO SALGRADE VALUES (2, 1201, 1400);
+INSERT INTO SALGRADE VALUES (3, 1401, 2000);
+INSERT INTO SALGRADE VALUES (4, 2001, 3000);
+INSERT INTO SALGRADE VALUES (5, 3001, 9999);
+--아직도 논리적인 저장만 진행
+commit; --물리적인 저장
+-- rollback; --커밋이 아닌 롤백시 트랜젝션 시작 부분으로 돌아감
+
