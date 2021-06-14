7454493db682015afd55b22a02673e2ab7fe205f
yiwoori <yiwoori93@naver.com>
2021-06-14 16:54:42 +0900

    Update 20210614_sql_subquery.sql

--- sql/20210614_sql_subquery.sql
+++ sql/20210614_sql_subquery.sql
@@ -5,5 +5,112 @@
 
 -- 스칼라 부속 질의 : select 절 이후에 사용
 -- 단일 행 단일 열 값이 사용된다.
-select o.custid, (select )
+select o.custid, (select c.name from customer c where o.custid=c.custid) as name
 from orders o;
+
+-- emp테이블만 사용해서 사원 이름, 부서 이름 출력
+select e.ename, (select d.dname from dept d where e.deptno=d.deptno) as dname
+from emp e;
+
+select ename, dname
+from emp, dept
+where emp.deptno=dept.deptno;
+
+-- 마당서점의
+-- 고객별 --group by
+-- 판매액을 출력
+-- 결과는 고객이름(customer)과 고객별 판매액
+select c.custid, c.name, sum(o.saleprice) as "판매액"
+from orders o, customer c
+where o.custid=c.custid
+group by c.custid, c.name;
+
+-- 스칼라 부속질의 사용
+select custid,
+    (select  name from customer c where o.custid=c.custid) as name,
+    sum(o.saleprice) as "구매액"
+from orders o
+group by custid;
+
+
+
+--인라인 뷰 : from 절 뒤에 사용되는 부속질의, 가상 테이브처럼 사용
+-- 고객번호가 2 이하인 -> custmer
+-- 고객의 판매액 -> orders
+-- 결과는 고객이름과 고객별 판매액 출력
+select o.custid, c.name, sum(o.saleprice)
+from(select * from customer where custid<=2) c, orders o
+where c.custid=o.custid
+group by o.custid, c.name;
+
+
+
+--중첩질의 : where절 뒤에 사용하는 부속질의
+-- 비교연산자를 이용할 때는 단일행 단일열의 결과를 갖는 부속질의를 사용
+-- > < = != >= <=
+
+-- 평균 급여보다 더 많은 급여를 받는 사원
+select *
+from emp
+--where sal > 1000 -- 1000 -> 평균급여
+where sal > (select avg(sal) from emp)
+order by sal;
+
+-- 평균 주문금액 이하의 주문에 대해서
+-- 주문번호와 금액을 보이시오  -->orders
+select *
+from orders
+where saleprice <= (select avg(saleprice) from orders)
+order by saleprice;
+--평균 주문 금액
+select avg(saleprice) from orders;
+
+--각 고객의 평균 주문 금액보다
+--큰 금액의 주문 내역에 대해서
+--주문번호, 고객번호, 금액을 보이시오
+select orderid, custid, saleprice
+from orders o1
+where saleprice > (select avg(saleprice) from orders o2 where o2.custid=o1.custid);
+
+select avg(saleprice) from orders o2 where o2.custid=o1.custid;
+select avg(saleprice) from orders where custid = 2;
+
+-- 다중행 연산자 IN
+--3000 이상 받는 사원이 소속된 부서와 동일한 부서에 근무하는 사원 리스트 출력
+select distinct deptno
+from emp
+where sal>=3000;    -- 3000 이상 받는 사원이 소속된 부서 출력
+select *
+from emp
+where deptno in(select distinct deptno from emp where sal>=3000);
+
+-- 대한민국에 거주하는 고객에게
+-- 판매한 도서의 총 판매액을 구하시오.
+select sum(saleprice)
+from orders
+where custid in (select custid from customer where address like '%대한민국%');
+select custid from customer where address like '%대한민국%';
+
+-- 3번 고객이 주문한 도서의 최고 금액
+-- 보다 더 비싼 
+-- 주문번호와 금액 출력
+select orderid, saleprice
+from orders
+where saleprice > (select max(saleprice) from orders where custid=3);
+--where saleprice > (select saleprice from orders where custid=3;);
+-- 3번 고객이 주문한 도서의 최고 금액
+select max(saleprice) from orders where custid=3;
+--다중행
+select saleprice from orders where custid=3;
+
+-- EXISTS 연산자로 대한민국에 거주하는 고객에게
+-- 판매한 도서의 총 판매액을 구하시오
+select sum(saleprice)
+from orders o
+where exists (select * from customer c where o.custid=c.custid
+    and c.address like '%대한민국%');
+
+select * from customer c where 1=c.custid and c.address like '%대한민국%';
+select * from customer c where 2=c.custid and c.address like '%대한민국%';
+select * from customer c where 3=c.custid and c.address like '%대한민국%';
+select * from customer c where 4=c.custid and c.address like '%대한민국%';
