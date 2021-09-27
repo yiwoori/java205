0c839e5e60ea3d56ef9bc306160fb121785b375e
yiwoori <yiwoori93@naver.com>
2021-09-27 23:36:58 +0900

    orl

--- spring_project/Orl-feed/src/main/java/com/bitcamp/orl/feed/controller/MountainFeedRestController.java
+++ spring_project/Orl-feed/src/main/java/com/bitcamp/orl/feed/controller/MountainFeedRestController.java
@@ -3,8 +3,7 @@ package com.bitcamp.orl.feed.controller;
 import com.bitcamp.orl.feed.domain.NewFeedList;
 import com.bitcamp.orl.feed.service.FeedManageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +17,7 @@ public class MountainFeedRestController {
 
     //피드 리스트 (최신순)
     @GetMapping("/feed/feedmain/selectNewMountainFeed")
+    @CrossOrigin
     public List<NewFeedList> newFeedList (String mName){
         System.out.println("rest 진입 산이름 : "+mName);
         List<NewFeedList> feedList = manageService.getNewFeedByMountain(mName);
